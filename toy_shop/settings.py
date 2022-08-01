5ce5fe63c32f3a645169749da9b646ae4638fa66
vera <lerika250293@mail.ru>
2022-08-01 09:54:22 +0300

    dont work static

--- toy_shop/settings.py
+++ toy_shop/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    "/static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
