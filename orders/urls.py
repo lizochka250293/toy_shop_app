4dd9c8532c859c767f0793ddca8cb442e6741905
vera <lerika250293@mail.ru>
2022-09-26 11:54:46 +0300

    add ordercreate

--- orders/urls.py
+++ orders/urls.py
@@ -7,5 +7,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('create/', OrderCreate.as_view(), name='order_create'),
+    path('created/', views.order_created, name='order_created'),
     path('add_pay/', views.add_pay, name='add_pay'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, FormView
 
 from app_toy_shop.models import Product
@@ -8,11 +10,12 @@ from .form import OrderCreateForm, AddPayForm
 from .models import OrderItem
 from .models import PayStatus
 
+# не понимаю как переопределить url если оплата картой
 class OrderCreate(FormView, ListView):
     model = Product
     template_name = 'orders/create.html'
     form_class = OrderCreateForm
-
+    success_url = reverse_lazy('orders:order_created')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,11 +24,11 @@ class OrderCreate(FormView, ListView):
 
     def get(self, request, *args, **kwargs):
         self.cart = Cart(request)
-        print(self.cart)
         for i in self.cart:
             cur_product = Product.objects.get(id=i['product'].id).quantity
             if cur_product <= i['quantity']:
                 info = 'Некоторые товары в Вашей корзине отсутвуют оформление не возможно'
+                return render(request, 'orders/create.html', {'info': info})
         self.object_list = self.get_queryset()
         context = self.get_context_data()
         return self.render_to_response(context)
@@ -34,25 +37,34 @@ class OrderCreate(FormView, ListView):
         self.cart = Cart(request)
         if not request.user.is_authenticated:
             return redirect('user:login_view')
+        for item in self.cart:
+            product = Product.objects.get(id=item['product'].id)
+            try:
+                total_quantity = product.quantity - item['quantity']
+                product.quantity = total_quantity
+                product.save()
+            except:
+                info = 'Некоторые товары в Вашей корзине отсутвуют оформление не возможно'
+                return render(request, 'orders/create.html', {'info': info})
         form = self.get_form()
         return self.form_valid(form)
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
+        print(self.get_form())
         self.object.user = self.request.user
-        print('obj', self.object.user)
-        print(self.cart)
         self.object.save()
-        print(self.object.id)
         for item in self.cart:
-            print(item)
             OrderItem.objects.create(order_id=self.object.id,
-                                    product=item['product'],
-                                    price=item['price'],
-                                    quantity=item['quantity'])
-        return super().form_valid(form)
+                                     product=item['product'],
+                                     price=item['price'],
+                                     quantity=item['quantity'])
+        self.cart.clear()
+        return HttpResponseRedirect(self.get_success_url())
 
 
+def order_created(request):
+    return render(request, 'orders/created.html')
 
 
 def order_create(request):
@@ -95,7 +107,7 @@ def order_create(request):
                             return redirect('orders:add_pay')
                         cart.clear()
                         return render(request, 'orders/created.html',
-                                          {'order': order, 'user': current_user})
+                                      {'order': order, 'user': current_user})
                 else:
                     form = OrderCreateForm()
                     return render(request, 'orders/create.html', {'cart': cart, 'form': form})
